c595b2e57b00cb365330ef7e491fb30ff63aae51
Miguel Julio <fitfab@yahoo.com>
2025-02-12 07:04:02 -0400

    feat: add SupaBase connection

--- app/components/carousel/partials.tsx
+++ app/components/carousel/partials.tsx
@@ -1,23 +1,44 @@
-import React from "react";
-import type { CarouselProps } from "./index";
+import React from "react"
+import type { CarouselProps } from "./index"
 
-export const CarouselViewport = ({ width, height, ...rest }: CarouselProps): React.ReactElement => {
-  const navigationHeight = "48px";
+export const CarouselViewport = ({
+  width,
+  height,
+  ...rest
+}: CarouselProps): React.ReactElement => {
+  const navigationHeight = "48px"
   const dynamicStyles = {
     width,
     height: `calc(${height} + ${navigationHeight})`,
-  };
-  return <div style={dynamicStyles} className={`relative mx-[auto] flex flex-wrap`} {...rest} />;
-};
-
-export const Steering = (props: React.HTMLAttributes<HTMLDivElement>): React.ReactElement => {
-  return <div className="flex flex-wrap justify-end content-center w-full pt-2" {...props} />;
-};
-
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  direction: "left" | "right";
+  }
+  return (
+    <div
+      style={dynamicStyles}
+      className={`relative mx-[auto] flex flex-wrap`}
+      {...rest}
+    />
+  )
 }
-export const Button = ({ direction, ...rest }: ButtonProps): React.ReactElement => {
+
+export const Steering = (
+  props: React.HTMLAttributes<HTMLDivElement>
+): React.ReactElement => {
+  return (
+    <div
+      className="flex flex-wrap justify-end content-center w-full pt-2"
+      {...props}
+    />
+  )
+}
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  direction: "left" | "right"
+}
+export const Button = ({
+  direction,
+  ...rest
+}: ButtonProps): React.ReactElement => {
   return (
     <button
       className="bg-neutral-500 hover:bg-neutral-900 disabled:bg-neutral-300 text-white p-2 rounded-full first:mr-4"
@@ -38,5 +59,5 @@ export const Button = ({ direction, ...rest }: ButtonProps): React.ReactElement 
         />
       </svg>
     </button>
-  );
-};
+  )
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,29 @@
-import { Carousel } from "~/components/carousel";
-import type { Route } from "./+types/home";
+import { Carousel } from "~/components/carousel"
+import { supabase } from "~/supabase-client"
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "React Router7" }, { name: "description", content: "Welcome to React Router 7" }];
+  return [
+    { title: "React Router7" },
+    { name: "description", content: "Welcome to React Router 7" },
+  ]
 }
 
-export default function Home() {
+export async function loader({ request }: Route.LoaderArgs) {
+  const { data } = await supabase.from("members").select("*")
+  return data
+}
+
+export async function action({ request }: Route.LoaderArgs) {
+  return { message: "Hello from action" }
+}
+
+export default function Home({
+  loaderData,
+  actionData,
+  params,
+  matches,
+}: Route.ComponentProps) {
   const data = [
     {
       src: "https://source.unsplash.com/480x320?sports",
@@ -104,24 +122,39 @@ export default function Home() {
       width: "480px",
       height: "320px",
     },
-  ];
+  ]
   return (
     <div className="">
       <p className="text-xl font-light text-decorator m-30 max-w-140">
-        Front-End Web Developer with a flair for design — committed to creating websites that meet design and technical
-        requirements — including SEO, Usability and accessibility based on web standards guidelines.
+        Front-End Web Developer with a flair for design — committed to creating
+        websites that meet design and technical requirements — including SEO,
+        Usability and accessibility based on web standards guidelines.
       </p>
-      <Carousel>
+      <h1>Welcome to My Route with Props!</h1>
+      <p>Loader Data: {JSON.stringify(loaderData, null, 2)}</p>
+
+      <p>Action Data: {JSON.stringify(actionData)}</p>
+      <p>Route Parameters: {JSON.stringify(params)}</p>
+      <p>Matched Routes: {JSON.stringify(matches)}</p>
+      <Carousel width="84%" height="432px">
         {data.map((card, index) => (
-          <div key={index} className="border-1 border-neutral-300 rounded-md p-6 w-[480px] overflow-hidden bg-white">
+          <div
+            key={index}
+            className="border-1 border-neutral-300 rounded-md p-6 w-[480px] overflow-hidden bg-white"
+          >
             <h2 className="text-1xl uppercase font-extrabold text-gray-600 underline  decoration-brand mb-4 decoration-4">
               {card.alt}
             </h2>
-            <img src={card.src} alt={card.alt} width={card.width} height={card.height} />
+            <img
+              src={card.src}
+              alt={card.alt}
+              width={card.width}
+              height={card.height}
+            />
             <p className="">{card.caption}</p>
           </div>
         ))}
       </Carousel>
     </div>
-  );
+  )
 }
