25a71ed5a34308de43fa5c0b5930573eb3754398
Miguel Julio <fitfab@yahoo.com>
2025-03-07 10:10:39 -0400

    refactor move layout to its folder

--- app/root.tsx
+++ app/root.tsx
@@ -7,8 +7,8 @@ import {
 } from "react-router"
 
 import type { Route } from "./+types/root"
-import AppLayout from "./app-layout"
 import stylesheet from "./app.css?url"
+import AppLayout from "./layout/app-layout"
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
